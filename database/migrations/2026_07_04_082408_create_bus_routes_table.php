962e1af6a2916b5d6f450f419de4caba394bd08b
ShoeheyOt <142355969+ShoeheyOt@users.noreply.github.com>
2026-07-05 22:50:45 +0900

    add travel_time to bus_routes table

--- database/migrations/2026_07_04_082408_create_bus_routes_table.php
+++ database/migrations/2026_07_04_082408_create_bus_routes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('bus_routes', function (Blueprint $table) {
             $table->id();
             $table->string("name");
+            $table->string("travel_time");
             $table->timestamps();
         });
     }
