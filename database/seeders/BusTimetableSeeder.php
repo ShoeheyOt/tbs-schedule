8d5c2556fed8bc0954dd5004f2833f81ec664048
ShoeheyOt <142355969+ShoeheyOt@users.noreply.github.com>
2026-07-10 22:48:29 +0900

    add seeder about 7am from nariai-nakamachi

--- database/seeders/BusTimetableSeeder.php
+++ database/seeders/BusTimetableSeeder.php
@@ -38,5 +38,41 @@ class BusTimetableSeeder extends Seeder
             'bus_route_id'  =>  3,   //Nariai to JR
             'departure_time' => '06:53:00'
         ]);
+        DB::table('bus_timetables')->insert([
+            'bus_route_id'  =>  3,   //Nariai to JR
+            'departure_time' => '07:01:00'
+        ]);
+        DB::table('bus_timetables')->insert([
+            'bus_route_id'  =>  3,   //Nariai to JR
+            'departure_time' => '07:07:00'
+        ]);
+        DB::table('bus_timetables')->insert([
+            'bus_route_id'  =>  3,   //Nariai to JR
+            'departure_time' => '07:14:00'
+        ]);
+        DB::table('bus_timetables')->insert([
+            'bus_route_id'  =>  3,   //Nariai to JR
+            'departure_time' => '07:22:00'
+        ]);
+        DB::table('bus_timetables')->insert([
+            'bus_route_id'  =>  3,   //Nariai to JR
+            'departure_time' => '07:28:00'
+        ]);
+        DB::table('bus_timetables')->insert([
+            'bus_route_id'  =>  3,   //Nariai to JR
+            'departure_time' => '07:36:00'
+        ]);
+        DB::table('bus_timetables')->insert([
+            'bus_route_id'  =>  3,   //Nariai to JR
+            'departure_time' => '07:43:00'
+        ]);
+        DB::table('bus_timetables')->insert([
+            'bus_route_id'  =>  3,   //Nariai to JR
+            'departure_time' => '07:50:00'
+        ]);
+        DB::table('bus_timetables')->insert([
+            'bus_route_id'  =>  3,   //Nariai to JR
+            'departure_time' => '07:58:00'
+        ]);
     }
 }
