5ba8fb5dc1f2e092fe73970abdeaced01ba69b20
ShoeheyOt <142355969+ShoeheyOt@users.noreply.github.com>
2026-07-17 21:25:41 +0900

    set the relation

--- app/Models/BusRoute.php
+++ app/Models/BusRoute.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class BusRoute extends Model
 {
-    //
+    protected $table = 'bus_routes';
+
+    /**
+     * Get the timetable that belongs to the route
+     */
+    public function busTimetable(): HasMany
+    {
+        return $this->hasMany(BusTimetable::class);
+    }
 }
--- app/Models/BusTimetable.php
+++ app/Models/BusTimetable.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BusTimetable extends Model
 {
-    //
+    protected $table = 'bus_timetables';
+
+    public function busRoute(): BelongsTo
+    {
+        return $this->belongsTo(BusRoute::class);
+    }
 }
