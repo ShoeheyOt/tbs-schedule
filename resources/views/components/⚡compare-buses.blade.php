750b3a6991bc6a46282a9541c4fa69b0232655f8
ShoeheyOt <142355969+ShoeheyOt@users.noreply.github.com>
2026-07-19 10:44:24 +0900

    display the routes in front view

--- resources/views/components/⚡compare-buses.blade.php
+++ resources/views/components/⚡compare-buses.blade.php
@@ -6,9 +6,13 @@ new class extends Component
 {
     public string $currentTime;
 
+    public $routes = [];
+
     public function mount()
     {
          $this->currentTime = now()->format('H:i');
+
+         $this->routes = DB::table('bus_routes')->get();
     }
 };
 ?>
@@ -20,4 +24,13 @@ new class extends Component
         <p style="margin: 0; color: #666;">現在の時刻</p>
         <strong style="font-size: 24px; color: #e03e2d;">{{ $currentTime }}</strong>
     </div>
+    <div style="margin: 20px; padding: 20px; background: #f5f5f5; border-radius: 8px; display: inline-block;">
+        <p style="margin: 0; color: #666;">routes</p>
+        @foreach ($routes as $route)
+            <li>
+                {{ $route->departure_point}}
+            </li>
+        @endforeach
+    </div>
+
 </div>
